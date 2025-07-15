067c7597fcb801c15aa6cba9c790a2eba9a69d5d
Ikram001 <ikramnixamani@gmail.com>
2025-07-15 13:32:43 +0500

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import './index.css'
+
+const App = () => {
+  return (
+    <div>
+      My Portfolio
+    </div>
+  );
+};
+
+export default App;
